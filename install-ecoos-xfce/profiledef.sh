e80bc36e5faea9306b198447b8ebf967ab80d861
rport99 <rport99@gmail.com>
2025-12-22 07:17:26 -0500

    ✨ Add eco_menu

--- install-ecoos-xfce/profiledef.sh
+++ install-ecoos-xfce/profiledef.sh
@@ -48,5 +48,6 @@ file_permissions=(
   ["/usr/local/bin/eco_pac"]="0:0:755"
   ["/usr/local/bin/eco_planner"]="0:0:755"
   ["/usr/local/bin/eco_exp"]="0:0:755"
+  ["/usr/local/bin/eco_menu"]="0:0:755"
   ["/usr/local/bin/browseros"]="0:0:755"
 )
